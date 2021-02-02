d5f90040eff98eb2a87061f2a6047855b0f03b94
Steven Gabbay <stevie_gabbay@hotmail.com>
2021-02-02 00:25:59 -0500

    Method for reading file, and possibly inserts to database, depending on dry run argument

--- user_upload.php
+++ user_upload.php
@@ -70,5 +70,95 @@
 			return True;
 		}
 	}
+	
+	//reads file and can call insert to database depeding on $dry run
+	function database ($dry){
+		global $file;
+		echo "Opening File: $file...\n";
+		$row = 1;
+		$accepted = -1;
+		$rejected = 0;
+		$read = @fopen($file, "r");
+		if ($read != FALSE){
+			echo "Reading File: $file\n\n";
+			while (($data = fgetcsv($read, 1000, ",")) != FALSE){
+				$fields = sizeof($data);
+				if($fields == 3){
+					$email = strtolower(trim($data[2]));
+					if (($row != 1) && !filter_var($email, FILTER_VALIDATE_EMAIL)) { //email validation
+						$rejected++;
+					}
+					else{						
+						$fname = ucfirst(strtolower(trim($data[0])));
+						$lname = ucfirst(strtolower(trim($data[1])));
+						if($row == 1){
+							$email = ucfirst($email);
+						}
+						else{
+							//Temp Variables
+							$fnameT = $fname;
+							$lnameT = $lname;
+							$emailT = $email;
+							$index = 0;
+							
+							for($x = 0; $x < strlen($fname); $x++){
+								if($fname[$x] == "'"){
+									$fname[$x+1] = strtoupper($fname[$x+1]);
+									$fnameT[$x+1+$index] = $fname[$x+1];
+									$fnameT = substr($fnameT,0,$x+$index) . "\\" . substr($fnameT,$x+$index);
+									$index++;
+								}
+							}
+							$index = 0;
+							for($x = 0; $x < strlen($lname); $x++){
+								if($lname[$x] == "'"){
+									$lname[$x+1] = strtoupper($lname[$x+1]);
+									$lnameT[$x+1+$index] = $lname[$x+1];
+									$lnameT = substr($lnameT,0,$x+$index) . "\\" . substr($lnameT,$x+$index);
+									$index++;
+								}
+							}
+							$index = 0;
+							for($x = 0; $x < strlen($email); $x++){
+								if($email[$x] == "'"){
+									$emailT = substr($emailT,0,$x+$index) . "\\" . substr($emailT,$x+$index);
+									$index++;
+								}
+							}
+						}
+						
+						if($row == 1){
+							echo "\t\t$fname, $lname, $email, ";
+							echo "Inserted to Database\n\n";
+						}
+						else{
+							if(!$dry){
+								if(!insert($fnameT, $lnameT, $emailT)){
+									$accepted--;
+									$rejected++;
+								}
+								else{
+									echo "\t\t$fname, $lname, $email, Yes\n";
+								}
+							}
+							else{
+								echo "\t\t$fname, $lname, $email, No\n";
+							}
+						}
+						$accepted++;
+					}
+				}
+				else{
+					$rejected++;
+				}
+				$row++;
+			}
+			echo "\n$accepted accepted entries, $rejected rejected entries\n";
+			fclose($read);
+		}
+		else{
+			echo "Could not open file: $file\n";
+		}
+	}
 
 ?>
